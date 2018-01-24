6b002883f0d12770cac6736785657aa62aeffbca
Milja <mmcvirtanen@gmail.com>
2018-01-24 17:59:05 +0100

    Initial Commit

---
+++ Labb2_Quiz/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Labb2_Quiz
+//
+//  Created by Milja Virtanen on 2018-01-24.
+//  Copyright © 2018 Milja Virtanen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
