b56b3b5dfe319f4ed44bbe812d59c6dc930ee40b
Milja <mmcvirtanen@gmail.com>
2018-01-24 20:23:47 +0100

    Layout mock-up and first methods.

---
+++ Labb2_Quiz/Quiz.h
@@ -0,0 +1,13 @@
+//
+//  Quiz.h
+//  Labb2_Quiz
+//
+//  Created by Milja Virtanen on 2018-01-24.
+//  Copyright © 2018 Milja Virtanen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Quiz : NSObject
+
+@end
---
+++ Labb2_Quiz/Quiz.m
@@ -0,0 +1,13 @@
+//
+//  Quiz.m
+//  Labb2_Quiz
+//
+//  Created by Milja Virtanen on 2018-01-24.
+//  Copyright © 2018 Milja Virtanen. All rights reserved.
+//
+
+#import "Quiz.h"
+
+@implementation Quiz
+
+@end
